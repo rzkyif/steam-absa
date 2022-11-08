7c7d9e98d639708ab8e5e9ba1b5c5b6137470f79
rzkyif <rykzius@gmail.com>
2022-11-08 20:24:07 +0700

    fix: remove deprecated code and add cors

--- be/main.py
+++ be/main.py
@@ -5,14 +5,6 @@ from engine import Engine
 engine = Engine('./db.sqlite')
 
 @hug.get('/api/query')
-def query(q: hug.types.cut_off(255), response = None):
-  if not engine.started: return json.dumps({
-    'error': 'Engine not started! Send POST request to /api/start first!'
-  })
-      
+def query(q: hug.types.cut_off(255), cors: hug.directives.cors = '*'):      
   result = engine.query(q)
   return json.dumps(result)
-
-@hug.post('/api/start')
-def query():
-  engine.start()
