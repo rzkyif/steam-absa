de25305a4bd8c4afd4b8311d113b0c346f6d7ad5
rzkyif <rykzius@gmail.com>
2022-11-15 11:40:28 +0700

    impr: modify initial sqlite3 database

--- data/raw2sqlite.py
+++ data/raw2sqlite.py
@@ -1,39 +1,34 @@
-# TODO: make code to convert all .jsonlines file to one .sqlite file
-import pandas as pd
-import numpy as np
-import nltk
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem.wordnet import WordNetLemmatizer 
-import stanfordnlp
 import sqlite3
+import json
+import os
+import sys
 
-def fun(x):
-    return x.replace("'","''")
+# configure which columns to take from json files
+DB_PATH = 'data/data.sqlite3'
+COLUMNS = ['username', 'review_url', 'review']
 
-# open all json
-names = ["Arma_3", "Counter_Strike_Global_Offensive", "Counter_Strike", "Dota_2", "Football_Manager_2015", "Garrys_Mod", "Grand_Theft_Auto_V", "Sid_Meiers_Civilization_5", "Team_Fortress_2", "The_Elder_Scrolls_V", "Warframe"]
-allreview = []
-for i in names:
-    name = "data/raw/"+i+".jsonlines"
-    df = pd.read_json(name, lines=True)
-    reviews = df['review'].tolist()
-    allreview += reviews
-# print(allreview)
-# print(allreview)
-allreview = list(map(fun, allreview))
-# print(c)
-# all = ",".join(c)
-# print(all)
-con = sqlite3.connect("data/reviews.db")
+# create sqlite database
+if os.path.exists(DB_PATH): 
+    check = input("Database already exists, remake? (y/N) ")
+    if check.lower() == 'y':
+        os.remove(DB_PATH)
+    else:
+        sys.exit('Database already exists.')
+con = sqlite3.connect(DB_PATH)
 cur = con.cursor()
+cur.execute(f"CREATE TABLE reviews(game_id, {', '.join(COLUMNS)})")
+cur.execute(f"CREATE TABLE game(name)")
 
-cur.execute("CREATE TABLE reviews(review)")
-
-params = {'allreview':allreview}
-for i in allreview:
-    print(f"INSERT INTO reviews VALUES ('{i}')")
-    cur.execute(f"INSERT INTO reviews VALUES ('{i}')")
-# cur.executemany("INSERT INTO reviews VALUES(?)", allreview)
+# open all review data, load into sqlite3 database
+games = ["Arma_3", "Counter_Strike_Global_Offensive", "Counter_Strike", "Dota_2", "Football_Manager_2015", "Garrys_Mod", "Grand_Theft_Auto_V", "Sid_Meiers_Civilization_5", "Team_Fortress_2", "The_Elder_Scrolls_V", "Warframe"]
+allreview = []
+print("Loading .jsonlines files...")
+for id, game in enumerate(games):
+    with open("data/raw/"+game+".jsonlines", 'r') as f:
+        print(f"  Processing {game} data...")
+        cur.execute(f"INSERT INTO game(rowid, name) VALUES (?, ?)", [id, game.replace('_', ' ')])
+        for line in f:
+            object = json.loads(line)
+            cur.execute(f"INSERT INTO reviews VALUES (?, ?, ?, ?)", [id] + [object[x] for x in COLUMNS])
 con.commit()
+print(f"Completed!")
