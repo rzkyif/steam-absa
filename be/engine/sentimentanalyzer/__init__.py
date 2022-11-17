eb3474a56c930109165db4ed5efd95e13609c53e
rzkyif <rykzius@gmail.com>
2022-11-17 15:48:05 +0700

    feat: partial sentiment analyzer

--- be/engine/sentimentanalyzer/__init__.py
+++ be/engine/sentimentanalyzer/__init__.py
@@ -1,3 +1,7 @@
+import spacy
+
+from engine import utilities
+
 class SentimentAnalyzer:
   """An class that will generate sentiment data from review data and aspect data.
   """
@@ -9,13 +13,15 @@ class SentimentAnalyzer:
         engine_db_conn (Connection): sqlite3 connection to the engine database
     """
 
+    print("Generating sentiment data...")
+    engine_db_conn.execute("DROP TABLE IF EXISTS sentiment")
     engine_db_conn.execute("""
       CREATE TABLE sentiment (
         review_id INTEGER,
         aspect_id INTEGER
         sentiment TEXT,
         FOREIGN KEY (review_id) 
-          REFERENCES review (rowid)
+          REFERENCES review (rowid) 
             ON DELETE CASCADE
             ON UPDATE NO ACTION,
         FOREIGN KEY (aspect_id) 
@@ -25,7 +31,34 @@ class SentimentAnalyzer:
       )
     """)
 
+    print("  Checking amod data...")
+    if utilities.table_exists(engine_db_conn, 'sentiment_amod'):
+      print("    Found existing amod data!")
+    else:
+      print("    Building new amod data...")
+      engine_db_conn.execute("""
+        CREATE TABLE sentiment_amod (
+          review_id INTEGER,
+          adjective TEXT
+          object TEXT,
+          FOREIGN KEY (review_id) 
+            REFERENCES review (rowid) 
+              ON DELETE CASCADE
+              ON UPDATE NO ACTION
+        )
+      """)
+      engine_db_conn.commit()
+      nlp = spacy.load("en_core_web_sm")
+      cur = engine_db_conn.cursor()
+      cur2 = engine_db_conn.cursor()
+      for review_data in cur.execute("SELECT rowid, review FROM review"):
+        [rowid, review] = review_data
+        doc = nlp(review)
+        for token in doc:
+          if token.dep_ != 'amod': continue
+          cur2.execute("INSERT INTO sentiment_amod VALUES (?, ?, ?)", [rowid, token.text, token.head.text])
+      engine_db_conn.commit()
+  
     # TODO: read 'review' table and 'aspect' table, populate 'sentiment' table with sentiment data
 
     engine_db_conn.commit()
-    
