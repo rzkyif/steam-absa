45f8b61802fe2cf33580e8e1eccf92341b2db438
Rizky I. F <rykzius@gmail.com>
2022-11-19 17:19:26 +0700

    fix: improve logging and cosine_sim performance

--- be/engine/informationretriever/__init__.py
+++ be/engine/informationretriever/__init__.py
@@ -1,4 +1,4 @@
-import sqlite3, numpy as np
+import numpy as np, time
 from sklearn.feature_extraction.text import TfidfVectorizer
 from numpy.linalg import norm
 
@@ -6,7 +6,7 @@ class InformationRetriever:
   """A class that will retrieve review data that corresponds to queries
   """
 
-  def retrieve(self, engine_db_conn, query):
+  def retrieve(self, engine_db_conn, query, count=20, skip=0, game_filter=None):
     """Retrieve review data in engine database that corresponds to the query
 
     Args:
@@ -16,40 +16,50 @@ class InformationRetriever:
     Returns:
         List[object]: list of review data that corresponds to query
     """    
-    
-    datas = []
-    comments = []
+
+    print(f"Processing query '{query}' ({count}, {skip}, {game_filter})...")
     cur = engine_db_conn.cursor()
-    for row in cur.execute("SELECT * from review"):
-      comment = row[3].replace("\\\'", "'")
-      data = (row[0], row[1], row[2], comment)
-      datas.append(data)
-      comments.append(comment)
+    row_ids = []
+    review_texts = []
+    for row in cur.execute("SELECT game_id, rowid, review from review"):
+      if game_filter is not None and row[0] not in game_filter: continue
+      row_ids.append(row[1])
+      review_texts.append(row[2].replace("\\\'", "'"))
 
+    print("  Making document and query vectors...")
     vec = TfidfVectorizer()
-    document_vec = vec.fit_transform(comments)
+    document_vec = vec.fit_transform(review_texts)
     query_vec = vec.transform([query])
-    query_array = np.array(query_vec.toarray()[0])
+    print("    Done!")
 
-    sims = []
-    for dv in document_vec:
-      document_array = np.array(dv.toarray())
-      sim = np.dot(document_array, query_array) / (norm(document_array) * norm(query_array)) if (norm(document_array) * norm(query_array)) != 0 else 0.0
-      sims.append(float(sim))
+    print("  Calculating cosine similarities...")
+    cosine_sims = []
+    y = np.array([query_vec.toarray()[0]])
+
+    document_count = len(row_ids)
+    start_time = time.perf_counter()
+    last_print = start_time - 10
+    for i, dv in enumerate(document_vec):
+      now = time.perf_counter()
+      if (now - last_print > 1) or (i == document_count-1):
+        spent_time = now - start_time
+        print(f"\r    Progress: {i+1}/{document_count} ({spent_time:.2f}s, ETA: {(document_count-(i+1))*(spent_time/(i+1)):.2f}s)", end="    ")
+        last_print = now
+
+      x = np.array(dv.toarray())
+
+      norms_X = np.sqrt((x * x).sum(axis=1))
+      x /= norms_X[:, np.newaxis]
+      norms_Y = np.sqrt((y * y).sum(axis=1))
+      y /= norms_Y[:, np.newaxis]
+      cosine_sim = np.dot(x, y.T)
+
+      cosine_sims.append(float(cosine_sim))
+    print()
     
-    # Tuple reviews: (game_id, username, review_url, review, cosine_similarity)
-    reviews = []
-    for i in range(len(datas)):
-      reviews.append((datas[i][0], datas[i][1], datas[i][2], datas[i][3], sims[i]))
+    print("  Calculating result row ids...")
+    reviews = [(row_ids[i], cosine_sims[i]) for i in range(len(row_ids))]
+    reviews.sort(key=lambda review: review[1], reverse=True)
+    print("    Done!")
 
-    reviews.sort(key=lambda tup: tup[4], reverse=True)
-    
-    for i in range(5):
-      print(reviews[i])
-
-    return reviews
-
-if __name__=='__main__':
-  ir = InformationRetriever()
-  con = sqlite3.connect('../../../data/data.sqlite3')
-  ir.retrieve(con, "gta")
+    return reviews[skip:skip+count]
